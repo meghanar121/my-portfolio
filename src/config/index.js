c99171281aeb73187a428ffa4be09e76a4b43589
meghana rajappa <meghanar121@gmail.com>
2020-10-22 19:53:02 +0200

    new updates

--- src/config/index.js
+++ src/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
         },
         {
             name: "CodePen",
-            url: "https://www.behance.net/"
+            url: "https://codepen.io/meghanar121"
         },
     ],
   
